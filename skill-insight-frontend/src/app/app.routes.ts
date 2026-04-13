b64d9a749d00ed100fda498e46792798d3d2f907
Vo Ke Vuong <vovuong535@gmail.com>
2026-04-13 12:57:20 +0700

    feat: setup frontend pages, routing and app config

--- skill-insight-frontend/src/app/app.routes.ts
+++ skill-insight-frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Student } from './pages/student/student';
+import { Teacher } from './pages/teacher/teacher';
+import { Admin } from './pages/admin/admin';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'student', component: Student },
+  { path: 'teacher', component: Teacher },
+  { path: 'admin', component: Admin }
+];
