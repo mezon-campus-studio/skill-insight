190b2fbfe73a9e51e8c5d7a22e5e671a159bfe56
choiieog <linkstart2004@gmail.com>
2026-04-24 12:41:27 +0700

    Fix: sửa syntax và cập nhật CSS

--- skill-insight-frontend/src/app/pages/login/login.ts
+++ skill-insight-frontend/src/app/pages/login/login.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: [
+    '../../../assets/form.css',
+    './login.css'
+  ],
+
 })
 export class Login {
+  user = {
+    email: '',
+    password: ''
+  };
 
+  onSubmit(form: any) {
+  }
 }
