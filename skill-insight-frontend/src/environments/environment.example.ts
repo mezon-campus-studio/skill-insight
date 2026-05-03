d5ee9833bb2e19309298427d1661dd71f56a6a32
dn0417284-rgb <dn0417284@gmail.com>
2026-05-03 23:54:17 +0700

    subject

--- skill-insight-frontend/src/environments/environment.example.ts
+++ skill-insight-frontend/src/environments/environment.example.ts
@@ -5,9 +5,6 @@ export const environment = {
   clientId: '',
   redirectUri: '',
   Oauth2_URL: '',
-<<<<<<< HEAD
   Auth_Mezon: '',
   API_SUBJECT: '',
-=======
->>>>>>> 28b0ebc9e6f79fff6c4204226989d07000425902
 };
